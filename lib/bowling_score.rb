19818585a4336f9a955d1be8d44d26cfa7b21503
Aisha-Yusuff <a.yusuff99@outlook.com>
2022-06-29 14:09:51 +0100

    Added spare counter to method and test and passed test

--- lib/bowling_score.rb
+++ lib/bowling_score.rb
@@ -3,6 +3,7 @@ class BowlingScore
     @scoresheet = scoresheet
     @spare = []
     @total = []
+    @spare_count = 0
   end
 
   def frames
@@ -14,16 +15,19 @@ class BowlingScore
   end
 
   def spare?
-    arr = @scoresheet.find{|frame| frame.sum == 10}
+    arr = @scoresheet.find{|frame| frame.sum == 10 && frame[0] != 10}
     if arr
       @spare.push(arr) 
       points = 10 + @spare[0][0]
       @total.push(points)
+      @spare_count = +1
+      p @spare
     end
   end
 
   def final_score
     final_score = @total.sum
-    "The final score for this game is #{final_score}. You scored 0 strikes and 0 spares."
+    spare_count = @spare_count
+    "The final score for this game is #{final_score}. You scored 0 strikes and #{spare_count} spares."
   end 
 end 
--- spec/bowling_score_spec.rb
+++ spec/bowling_score_spec.rb
@@ -2,13 +2,13 @@ require "bowling_score"
 
 describe BowlingScore do 
   context "Gutter game" do 
-    it "it returns total score = 0 when player scores no points in game" do 
+    it "it returns the total score = 0 when player scores no points in game" do 
       scoresheet = BowlingScore.new([[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0][0,0],[0,0],[0,0]])
       expect(scoresheet.final_score).to eq ("The final score for this game is 0. You scored 0 strikes and 0 spares.")
     end 
 
     context "player scores points in only one frame" do 
-      it "it returns total score and number of spares and strikes the player scored" do 
+      it "it returns the total score, 0 for the number of spares and strikes the player scored" do 
         scoresheet = BowlingScore.new([[0,0],[3,2],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]])
         scoresheet.frames
         scoresheet.spare?
@@ -17,10 +17,11 @@ describe BowlingScore do
     end
 
     context "player scores a spare in only one frame" do 
-      it "it returns total score and number of spares and strikes the player scored" do 
+      it "it returns the total score, 0 strikes and 1 spares scored" do 
         scoresheet = BowlingScore.new([[0,0],[3,7],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]])
+        scoresheet.frames
         scoresheet.spare?
-        expect(scoresheet.final_score).to eq ("The final score for this game is 13. You scored 0 strikes and 0 spares.")
+        expect(scoresheet.final_score).to eq ("The final score for this game is 13. You scored 0 strikes and 1 spares.")
       end
     end
   end
