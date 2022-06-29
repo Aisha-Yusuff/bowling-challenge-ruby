3fcaf723cf0ee6c71f350f5e40c5e38ddd24f6df
Aisha-Yusuff <a.yusuff99@outlook.com>
2022-06-29 13:45:25 +0100

    Created frames method, passed third test and edited second test

--- lib/bowling_score.rb
+++ lib/bowling_score.rb
@@ -1,11 +1,29 @@
 class BowlingScore
   def initialize(scoresheet)
     @scoresheet = scoresheet
+    @spare = []
+    @total = []
+  end
+
+  def frames
+    arr = @scoresheet.select{|frame| frame.sum < 10}
+    if arr
+      points = arr.flatten.sum
+      @total.push(points)
+    end
+  end
+
+  def spare?
+    arr = @scoresheet.find{|frame| frame.sum == 10}
+    if arr
+      @spare.push(arr) 
+      points = 10 + @spare[0][0]
+      @total.push(points)
+    end
   end
 
   def final_score
-    total_array = @scoresheet.flatten
-    total = total_array.sum
-    "The final score for this game is #{total}. You scored 0 strikes and 0 spares."
+    final_score = @total.sum
+    "The final score for this game is #{final_score}. You scored 0 strikes and 0 spares."
   end 
 end 
--- spec/bowling_score_spec.rb
+++ spec/bowling_score_spec.rb
@@ -7,10 +7,20 @@ describe BowlingScore do
       expect(scoresheet.final_score).to eq ("The final score for this game is 0. You scored 0 strikes and 0 spares.")
     end 
 
-    context "player scores point in one frame" do 
+    context "player scores points in only one frame" do 
       it "it returns total score and number of spares and strikes the player scored" do 
-        scoresheet = BowlingScore.new([[0,0],[3,2],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0][0,0],[0,0],[0,0]])
+        scoresheet = BowlingScore.new([[0,0],[3,2],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]])
+        scoresheet.frames
+        scoresheet.spare?
         expect(scoresheet.final_score).to eq ("The final score for this game is 5. You scored 0 strikes and 0 spares.")
+      end
+    end
+
+    context "player scores a spare in only one frame" do 
+      it "it returns total score and number of spares and strikes the player scored" do 
+        scoresheet = BowlingScore.new([[0,0],[3,7],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]])
+        scoresheet.spare?
+        expect(scoresheet.final_score).to eq ("The final score for this game is 13. You scored 0 strikes and 0 spares.")
       end
     end
   end
