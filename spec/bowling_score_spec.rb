66ad9287115cc9697752fea7333924a164e104c6
Aisha-Yusuff <a.yusuff99@outlook.com>
2022-06-29 12:32:52 +0100

    Created a second test and test passed

--- spec/bowling_score_spec.rb
+++ spec/bowling_score_spec.rb
@@ -6,5 +6,12 @@ describe BowlingScore do
       scoresheet = BowlingScore.new([[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0][0,0],[0,0],[0,0]])
       expect(scoresheet.final_score).to eq ("The final score for this game is 0. You scored 0 strikes and 0 spares.")
     end 
+
+    context "player scores point in one frame" do 
+      it "it returns total score and number of spares and strikes the player scored" do 
+        scoresheet = BowlingScore.new([[0,0],[3,2],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0][0,0],[0,0],[0,0]])
+        expect(scoresheet.final_score).to eq ("The final score for this game is 5. You scored 0 strikes and 0 spares.")
+      end
+    end
   end
 end 
